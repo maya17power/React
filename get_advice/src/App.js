19222bf188798b70cf61bc1adc3af3d6dd4d73fd
WildStyle <9sanchez@protonmail.com>
2024-01-02 21:45:57 -0800

    updated get advice app.

--- get_advice/src/App.js
+++ get_advice/src/App.js
@@ -1,59 +1,59 @@
-//useState required when updating element inner values
-//useEffect required when to load default values into element on load.
 import { useEffect, useState } from "react";
 
-//App component contains main function which generates html elemets programatically
-export default function App() {
-  //create a new state to update dom element inner values
-  //perameter "adv" is the value to be placed in dom element as value
-  //perameter "setAdvice" is what wraps the data to be used by "adv" perameter
-  //useState perameter is the default string
-  const [adv, setAdvice] = useState("");
-  const [count, setCount] = useState(0);
-  const rn = Math.floor(Math.random() * 100);
+function App(){
+const [patchLogo, getPatch] = useState("");
+const [flightNumber, getFN] = useState("");
+const [date, getDate] = useState("");
+const [name, getName] = useState("");
+const [crew, getCrew] = useState("");
 
-  //async function fetches api data in json format.
-  //async requires a function to return a promise -( Producing Code & Consuming Code)
-  async function getAdvice() {
-    //await requires a function to return a promise (Producing Code)
-    const response = await fetch("https://api.adviceslip.com/advice/" + rn);
-    //await requires a function to return a promise (Consuming Code)
-    const rawData = await response.json();
-    //example filter: rawData.slip["advice"]
-    //example filter: rawData['slip']['advice']
-    //example filter: rawData.slip.advice
-    //example filter: rawData.slip.id
-    //example filter: rawData['slip']['id']
-    //example filter: rawData.slip["id"]
+const crewRoles = [...crew];
+console.log(crewRoles.map((d)=> d.role));
 
-    console.log("Random Number:" + rn + ", Slip_ID: " + rawData.slip.id);
+    async function getData(){
+        const response = await fetch("https://api.spacexdata.com/v5/launches/latest");
+        const data = await response.json();
+        getPatch(data.links.patch.small);
+        getFN(data.flight_number);
+        getDate(data.date_local);
+        getName(data.name);
+        getCrew(data.crew);
 
-    setAdvice(rawData.slip.advice);
-    setCount((c) => c + 1);
-  }
+        /*
+        data.links.patch.small
+        data.flight_number
+        data.date_local
+        data.name
+        data.crew
+        */
 
-  //assign function into useEffect method with second argument as empty array.
-  useEffect(() => {
-    getAdvice();
-  }, []);
+        //console.log(getCrew());
+        
 
-  return (
-    <div>
-      <h1>{adv}</h1>
-      {/* add onClick perameter to button element */}
-      <button onClick={getAdvice}>Get Advise</button>
-      {/*add 'Message' component tag function name with prop which is variable count value. */}
-      <Message count={count} />
-    </div>
-  );
+    };
 
-  //Message component handles messages to end user.
-  //props perameter is collected to utilize useState within function.
-  function Message(props) {
-    return (
-      <p>
-        You have read <strong>{props.count}</strong> pieces of advice.
-      </p>
+    useEffect(()=>{
+        //this function will update in the DOM
+        //fetch something
+        getData();
+    },[]);
+
+    //return from app
+    return(
+        <div>
+            <h1>SpaceX info</h1>
+            <h2><img src={patchLogo} alt="SpaceX Crew 5 Patch Logo"></img></h2>
+            <ul>
+                <li>Flight Number: {flightNumber}</li>             
+                <li>Date: {date}</li>             
+                <li>Group Name: {name}</li>             
+                <li>Crew:<ul>
+                            
+                         </ul>
+                </li>                   
+            </ul>
+        </div>
     );
-  }
-}
+};
+
+export default App;
---
+++ get_advice/src/App2.js
@@ -0,0 +1,59 @@
+//useState required when updating element inner values
+//useEffect required when to load default values into element on load.
+import { useEffect, useState } from "react";
+
+//App component contains main function which generates html elemets programatically
+export default function App() {
+  //create a new state to update dom element inner values
+  //perameter "adv" is the value to be placed in dom element as value
+  //perameter "setAdvice" is what wraps the data to be used by "adv" perameter
+  //useState perameter is the default string
+  const [adv, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+  const rn = Math.floor(Math.random() * 300);
+
+  //async function fetches api data in json format.
+  //async requires a function to return a promise -( Producing Code & Consuming Code)
+  async function getAdvice() {
+    //await requires a function to return a promise (Producing Code)
+    const response = await fetch("https://api.adviceslip.com/advice/" + rn);
+    //await requires a function to return a promise (Consuming Code)
+    const rawData = await response.json();
+    //example filter: rawData.slip["advice"]
+    //example filter: rawData['slip']['advice']
+    //example filter: rawData.slip.advice
+    //example filter: rawData.slip.id
+    //example filter: rawData['slip']['id']
+    //example filter: rawData.slip["id"]
+
+    console.log("Random Number:" + rn + ", Slip_ID: " + rawData.slip.id);
+
+    setAdvice(rawData.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  //assign function into useEffect method with second argument as empty array.
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>{adv}</h1>
+      {/* add onClick perameter to button element */}
+      <button onClick={getAdvice}>Get Advise</button>
+      {/*add 'Message' component tag function name with prop which is variable count value. */}
+      <Message count={count} />
+    </div>
+  );
+
+  //Message component handles messages to end user.
+  //props perameter is collected to utilize useState within function.
+  function Message(props) {
+    return (
+      <p>
+        You have read <strong>{props.count}</strong> pieces of advice.
+      </p>
+    );
+  }
+}
